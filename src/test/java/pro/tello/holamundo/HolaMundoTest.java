7d79a581cb34e4a6b60123789769151daedb4ba4
Fran Tello <jfrantello@gmail.com>
2014-03-05 22:59:47 +0100

    Eliminado import no usado

--- src/test/java/pro/tello/holamundo/HolaMundoTest.java
+++ src/test/java/pro/tello/holamundo/HolaMundoTest.java
@@ -4,11 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import pro.tello.holamundo.HolaMundo;
 
 @RunWith(JUnit4.class)
 public class HolaMundoTest  {
