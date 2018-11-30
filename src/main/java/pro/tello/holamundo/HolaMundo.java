6eb9c15badf3e88e7c3166e940f54a0ee8094126
sdeprada <40704901+sdeprada@users.noreply.github.com>
2018-11-30 14:30:48 +0100

    codigo bueno

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -6,12 +6,10 @@ public class HolaMundo {
 	public static final String SALUDO2 = "Hola USAL!";
 	
 	public String saludar(int s) {
-		String saludo;
 		if ( s == 1 ) {
 			return HolaMundo.SALUDO1;
 		} else {
 			return HolaMundo.SALUDO2;
 		}
-		//return saludo;
 	}
 }
