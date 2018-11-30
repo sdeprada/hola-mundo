cdb24ce3ed0135183bc5c20bb102882a0281650a
sdeprada <40704901+sdeprada@users.noreply.github.com>
2018-11-30 13:56:05 +0100

    condicion

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,9 +2,16 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
-	public static final String SALUDO = "Hola a todos!";
+	public static final String SALUDO1 = "Hola a todos!";
+	public static final String SALUDO2 = "Hola USAL!";
 	
-	public String saludar() {
-		return HolaMundo.SALUDO;
+	public String saludar(int s) {
+		String saludo;
+		if ( s == 1 ) {
+			saludo = HolaMundo.SALUDO1;
+		} else {
+			saludo = HolaMundo.SALUDO2;
+		}
+		return saludo;
 	}
 }
