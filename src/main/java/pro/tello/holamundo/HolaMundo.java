e08d824478e0c99bf81f4dafc2400d69865f0eb8
sdeprada <40704901+sdeprada@users.noreply.github.com>
2018-11-30 10:45:20 +0100

    constante comentada

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,7 +2,7 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
-	public static final String SALUDO = "Hola, Mundo!";
+	//public static final String SALUDO = "Hola, Mundo!";
 
 	public String saludar() {
 		//return HolaMundo.SALUDO;
