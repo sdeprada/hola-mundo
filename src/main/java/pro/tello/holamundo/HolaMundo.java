61b7d24a5322cd62caa103bbeffdb4b76c412b02
sdeprada <40704901+sdeprada@users.noreply.github.com>
2018-11-30 13:35:27 +0100

    ej 1

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,10 +2,7 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
-	//public static final String SALUDO = "Hola, Mundo!";
-
 	public String saludar() {
-		//return HolaMundo.SALUDO;
 		return "Hola, Mundo!";
 	}
 }
