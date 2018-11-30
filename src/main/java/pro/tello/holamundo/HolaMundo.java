4b581df1b15fba13e7c4cede95b0758773209493
sdeprada <40704901+sdeprada@users.noreply.github.com>
2018-11-30 19:35:14 +0100

    metodo saludar sin smell

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,10 +2,15 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
+	public static final String SALUDO1 = "Hola a todos!";
+	public static final String SALUDO2 = "Hola USAL!";
+	
 	public String saludar(int s) {
-		
-		
-		return "Hola";
+		if ( s == 1 ) {
+			return HolaMundo.SALUDO1;
+		} else {
+			return HolaMundo.SALUDO2;
+		}
 		
 	}
 }
