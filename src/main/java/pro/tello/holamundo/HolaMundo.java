49e4f6e1ec0cdf2ea4f8898f4531cbefc0c68259
sdeprada <sdeprada@gmail.com>
2018-12-19 09:19:23 +0100

    ej1

--- src/main/java/pro/tello/holamundo/HolaMundo.java
+++ src/main/java/pro/tello/holamundo/HolaMundo.java
@@ -2,16 +2,21 @@ package pro.tello.holamundo;
 
 public class HolaMundo {
 	
-	public static final String SALUDO1 = "Hola a todos!";
-	public static final String SALUDO2 = "Hola VIEWNEXT!";
+	//public static final String SALUDO1 = "Hola a todos!";
+	//public static final String SALUDO2 = "Hola VIEWNEXT!";
 	
 	
 	public String saludar(int s) {
-		if ( s == 1 ) {
-			return HolaMundo.SALUDO1;
-		} else {
-			return HolaMundo.SALUDO2;
+		String saludo;
+		switch ( s ) {
+			case 1:
+				saludo =  "hola 1";
+			case 2: 
+				saludo =  "hola 2";
+			default:
+				saludo =  "hola 2";
 		}
+		return saludo;
 		
 	}
 }
