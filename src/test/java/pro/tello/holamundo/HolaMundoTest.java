36f9c82c9c4f94752551365a1e8a1c1a6c561687
sdeprada <sdeprada@gmail.com>
2018-12-19 14:02:07 +0100

    ej1.1

--- src/test/java/pro/tello/holamundo/HolaMundoTest.java
+++ src/test/java/pro/tello/holamundo/HolaMundoTest.java
@@ -22,5 +22,11 @@ public class HolaMundoTest  {
 		String saludo = holaMundo.saludar(1);
 		assertTrue( null != saludo && !("".equals(saludo)));
 	}
+
+	@Test
+	public void saludosParaDos() {
+		String saludo = holaMundo.saludar(2);
+		assertTrue( null != saludo && !("".equals(saludo)));
+	}
 	
 }
